3eb9b6e9dc8ebb34faf7a4e5641e117c9690a816
gannapureddyswetha <39914698+gannapureddyswetha@users.noreply.github.com>
2018-07-14 08:50:11 -0400

    Update development.rb

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "192.168.33.20", user: "vagrant", roles: %w{app db web}
+server "192.168.33.20", user: "vagrant", password: "vagrant", roles: %w{app db web}
 
 # role-based syntax
 # ==================
